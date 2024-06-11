a0ad40252c0fed36dc10bc225c37388bd802aff9
devteam0099 <av.dev2024@gmail.com>
2024-06-11 17:13:55 +0500

    added

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ import dbconnection from './utils/db.connection.js';
 import cloudinaryConfig from './utils/cloudinary.config.js';
 import corsConfig from './utils/cors.config.js';
 import router from './routes/webroutes.js';
-import { upload } from './middlewares/sample.middleware.js';
- import uploadImage  from './utils/image.uloader.js';
- import savedatatodb from './utils/datasave.db.js';
+
 const app = express();
 const port = 3000;
 
@@ -17,17 +15,6 @@ dbconnection();
 cloudinaryConfig();
 
 app.use('/',router)
-
-//  app.post('/',upload.array('images',3), async(req,res)=>{
-
-//   const {username} = req.body
-//   const imageurls =  await uploadImage(req.files)
-//   savedatatodb(imageurls, username)
-   
-//  })
-
-
-
 
 
 app.listen(port, () => {
